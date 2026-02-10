5c563aaae8c26fd1946d12863cbb6307ee4f6aa4
Dharshith Dev <dharshithdevkp@gmail.com>
2026-02-10 21:43:59 +0530

    Req

--- ai-rag/main.py
+++ ai-rag/main.py
@@ -12,8 +12,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://ai-knowledge-assistant-nine.vercel.app", 
-        "http://localhost:5173"                          
+        "*"                          
     ],
     allow_methods=["*"],
     allow_headers=["*"],
